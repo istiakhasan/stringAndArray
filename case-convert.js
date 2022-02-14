566423e23b159535a685e50ac86ec715393921b6
Istieak <85983888+Istieak@users.noreply.github.com>
2022-02-14 20:13:46 +0600

    case-convert function -toLowerCase() -toUpperCase

---
+++ case-convert.js
@@ -0,0 +1,9 @@
+const anthem='amar shonar Bangla ami toami valobashi'
+const search='valo';
+const userInput='blackPinK';
+const savedUserName='blackPink';
+if(userInput.toLowerCase()==savedUserName.toLowerCase()){
+    console.log('user exist')
+}else{
+    console.log("did not match")
+}
