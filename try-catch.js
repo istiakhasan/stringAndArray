baa9e7c7cd943afca018320015da39ad244ed39f
Istieak <85983888+Istieak@users.noreply.github.com>
2022-02-16 13:39:28 +0600

    try catch

---
+++ try-catch.js
@@ -0,0 +1,17 @@
+const name='ami';
+const student=undefined;
+
+// for(let i=0;i<10 i++){
+
+// }
+// name='tumi';
+// console.log(name)
+
+try{
+name='tumi'
+}catch(error){
+ console.log('error')
+}finally{
+    
+}
+console.log(name)
